96953efeb507947db76d7c563c691f050f957b93
Jose Carpio <jose.carpio@gmail.com>
2020-04-08 12:36:14 +0200

    session until minute 3:22

---
+++ introOpenGL/session2/source/OpenGLTutorialProject/Display.cpp
@@ -0,0 +1,13 @@
+#include "Display.h"
+#include <iostream>
+
+Display::Display(int width, int height, const std::string& title)
+{
+	std::cout << "Constructor!" << std::endl;
+}
+
+
+Display::~Display()
+{
+	std::cout << "Distructor!" << std::endl;
+}
---
+++ introOpenGL/session2/source/OpenGLTutorialProject/Display.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <string>
+
+
+class Display
+{
+public:
+	Display(int width, int height, const std::string& title);
+
+	virtual ~Display();
+
+protected:
+
+private:
+	Display(const Display& other) {}
+	Display& operator=(const Display& other) {}
+};
+
+#endif // DISPLAY_H
