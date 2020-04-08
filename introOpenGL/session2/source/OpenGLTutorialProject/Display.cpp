e3562a57cd1d6dee0e7f2ecb06b947f171f6fb1e
Jose Carpio <jose.carpio@gmail.com>
2020-04-08 13:31:55 +0200

    session 2 until minute 17:07

--- introOpenGL/session2/source/OpenGLTutorialProject/Display.cpp
+++ introOpenGL/session2/source/OpenGLTutorialProject/Display.cpp
@@ -1,13 +1,34 @@
 #include "Display.h"
+#include <GL/glew.h>
 #include <iostream>
 
-Display::Display(int width, int height, const std::string& title)
-{
-	std::cout << "Constructor!" << std::endl;
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA, 8); # Transparency
+	# 32 bits for color data
+
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32); # Bits for a single picture
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER_SIZE, 1); # we need space for a second window
+
+	m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, with, height, SDL_WINDOW_OPENGL);
+	m_glContext = SDL_GL_CreateContext(m_window);
+
+	GLenum status = glewInit();
+
+	if (status != GLEW_OK) {
+		std::cerr << "Glew failed to initialize!" << sdt::endl;
+	}
+	    
 }
 
 
 Display::~Display()
 {
-	std::cout << "Destructor!" << std::endl;
+	SDL_GL_DeleteContext(m_glContext);
+	SDL_DestroyWindow(m_window);
+	SDL_Quit();
+}
+
+void Display::SwapBuffers() 
+{
+   SDL_GL_SwapWindow(m_window);
 }
--- introOpenGL/session2/source/OpenGLTutorialProject/Display.h
+++ introOpenGL/session2/source/OpenGLTutorialProject/Display.h
@@ -2,6 +2,7 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <SDL2/SDL.h>
 #include <string>
 
 
@@ -10,6 +11,8 @@ class Display
 public:
 	Display(int width, int height, const std::string& title);
 
+	void SwapBuffers();
+
 	virtual ~Display();
 
 protected:
@@ -17,6 +20,9 @@ protected:
 private:
 	Display(const Display& other) {}
 	Display& operator=(const Display& other) {}
+
+	SDL_Window* m_window;
+	SDL_GLContext m_glContext;
 };
 
 #endif // DISPLAY_H
