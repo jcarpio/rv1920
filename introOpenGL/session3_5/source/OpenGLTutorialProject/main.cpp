2c71fcb9a04dd0683548e6f35e61979cc2705997
Jose Carpio <jose.carpio@gmail.com>
2020-04-09 11:06:46 +0200

    final session 3.5

--- introOpenGL/session3_5/source/OpenGLTutorialProject/main.cpp
+++ introOpenGL/session3_5/source/OpenGLTutorialProject/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "shader.h"
 
 
 
@@ -12,10 +13,13 @@ int main()
 {
 	Display display(800, 600, "Hello World!");
 
+	Shader shader("./res/basicShader");
+
 	while(!display.IsClosed())
 	{
 		glClearColor(0.0f, 0.15f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		
+		shader.Bind();
 
 		display.Update();
 
